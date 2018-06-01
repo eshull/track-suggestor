d2c43bbe2f6b3cf9931e5188e71e6ae7f97254bd
Eric Shull <eshulldesign@gmail.com>
2018-06-01 10:02:59 -0700

    add first form

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
   $(function(){
     $("#epicodusTrack").submit(function(event) {
       event.preventDefault();
-      var height = parseInt($("input:radio[name=interest]:checked").val());
+      var work = parseInt($("input:radio[name=interest]:checked").val());
       $(".parentDiv").show();
       $(".childOne").hide();
       $(".childTwo").hide();
@@ -21,8 +21,8 @@
   });
 
 // questions and answers for form
-// Where do you want to work? startup, small company, large corporation
-// What Interests you most? how things look, how things work, both
-// What type of development interests you? back end, front end, both
-// are you a windows or mac person? windows, mac, either
-// what types of development projects do you like? Business projects, mobile app, both business and mobile
+// work || Where do you want to work? startup, small company, large corporation
+// interest || What Interests you most? how things look, how things work, both
+// development || What type of development interests you? back end, front end, both
+// computer || are you a windows or mac person? windows, mac, either
+// projects || what types of development projects do you like? Business projects, mobile app, both business and mobile
